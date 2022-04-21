06a563ba3b6b3def99f4ae4bdc955a44c2f6e104
chaitu2022 <104137613+chaitu2022@users.noreply.github.com>
2022-04-21 16:57:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit
 streamlit.title ("My FIRST APP")
 streamlit.title("my Husbands\'s healthy Breakfst")
 streamlit.header("BreakFast Menu")
-Streamlit.text('Dosa with Tomoto Chunty')
-
-
-
+streamlit.text('Dosa with Tomoto Chunty')
+streamlit.text('vada with chicken Chunty')
+streamlit.text('rice ith egg Chunty')
