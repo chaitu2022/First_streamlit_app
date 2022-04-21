081ae7f292dddf56bf72ba256985eeda8c9a1c07
chaitu2022 <104137613+chaitu2022@users.noreply.github.com>
2022-04-21 16:47:54 +0530

    Update streamlit_app.py
    I have added breakfast details

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 streamlit.title ("My FIRST APP")
 streamlit.title("my Husbands\'s healthy Breakfst")
+streamlit.title("BreakFast Menu")
+Streamlit.title(" Dosa with \& Tomoto Chunty")
+Streamlit.title(" vada with \& Chicken Curry")
+Streamlit.title(" rice with \& lemon")
+
