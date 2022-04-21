f773a1786ae7e3b704b7675f3cab68f6c80d8c87
chaitu2022 <104137613+chaitu2022@users.noreply.github.com>
2022-04-21 17:46:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@
 import streamlit
 streamlit.title ('My FIRST APP')
 streamlit.title('my Husbands\'s healthy Breakfst')
-streamlit.header('BreakFast Menu')
-streamlit.text('Dosa with Tomoto Chunty')
-streamlit.text('vada with chicken Chunty')
-streamlit.text('rice with egg Chunty')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
