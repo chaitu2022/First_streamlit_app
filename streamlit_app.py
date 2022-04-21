8207d67496c9a6b181e26cc60328ff28730858ac
chaitu2022 <104137613+chaitu2022@users.noreply.github.com>
2022-04-21 16:58:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 ## CREATING NEW STREMLIT_APP.PY FOR SNOWFLAKE DEMO
 import streamlit
-streamlit.title ("My FIRST APP")
-streamlit.title("my Husbands\'s healthy Breakfst")
-streamlit.header("BreakFast Menu")
+streamlit.title ('My FIRST APP')
+streamlit.title('my Husbands\'s healthy Breakfst')
+streamlit.header('BreakFast Menu')
 streamlit.text('Dosa with Tomoto Chunty')
 streamlit.text('vada with chicken Chunty')
-streamlit.text('rice ith egg Chunty')
+streamlit.text('rice with egg Chunty')
